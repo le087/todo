cb7abe81c997c722f812f1bae58daa93b4c656d4
Mak Tomilov <mak.tomilov@gmail.com>
2012-09-08 15:09:35 +0600

    first commit

---
+++ todo.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, json
+from flask import Flask, request, render_template
+
+def read_file(source_file):
+    """open file for get list of TODO in dictionary
+    Arguments:
+    - `source_file`: source file
+    """
+    try:
+        return json.load(open(source_file, 'r'))
+    except:
+        return False
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+        
+@app.route('/', methods=['POST'])
+def index():
+    """view for user"""
+    return render_template('index.html')   
+
+if __name__ == "__main__":
+    app.run(debug=True)
