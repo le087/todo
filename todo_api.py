c3f4b2f2cf7e0000ae4c55652c9cc0e925b7ebb3
Mak Tomilov <mak.tomilov@gmail.com>
2012-09-09 17:03:02 +0600

    проверил работу сокета

--- todo_api.py
+++ todo_api.py
@@ -6,29 +6,35 @@ import uuid
 import json
 import socket
 
-#configuration
-DATADIR = os.path.abspath('.') + '/user_data'
-
 class todoserver:
     """создает сервер, который слушает по протоколу 
     подобному телнету пор и предоставляет информацию
     по спискам заданий для того или иного пользователя
     """
     
-    def __init__(self):
+    def __init__(self, adress='192.168.0.2', port=12235):
         """ конструктор
         """
-        self.adress = '0.0.0.0'
-        self.port = 12235
+        self.ADRESS = adress
+        self.PORT = port
+        self.DATADIR = os.path.dirname(__file__) + '/user_data'
         
-    def run(self, adress, port):
+    def run(self):
         """ Метод открывает сокет и слушает его на
         указанном порту
         Arguments:
         - `self`:
         """
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.bind((self.ADRESS, self.PORT))
+        server_sock.listen(5)
+        
         while 1:
-            pass
+            conn, adr_user = server_sock.accept()
+            while 1:
+                data = conn.recv(1024)
+                print data
+            conn.close()
 
     def get_list_user(self):
         """ возвращает список пользователей
@@ -46,9 +52,9 @@ class todoserver:
         - `self`:
         - `user`: пользователь, для которого необходим вывести инфу
         """
-        list_user = os.listdir(DATADIR)
+        list_user = os.listdir(self.DATADIR)
         if user in list_user:
-            data = json.load(open(DATADIR + user, 'r'))
+            data = json.load(open(self.DATADIR + user, 'r'))
             return [(k, v) for k, v in data.items()]
         else:
             return False
@@ -62,16 +68,18 @@ class todoserver:
         - `user`: пользоветель
         - `todo`: новая запись
         """
-        list_user = os.listdir(DATADIR)
+        data = json.load(open(self.DATADIR + user, 'r'))
+        data[str(uuid.uuid1())] = todo
+        list_user = os.listdir(self.DATADIR)
         if user in list_user:
-            data = json.dump(open(DATADIR + user, 'r'))
-            return [(k, v) for k, v in data.items()]
+            json.dump(data, open(self.DATADIR + user, 'wb'))
+            return True
         else:
             return False
         
 
             
-
+app = todoserver()
 
 if __name__ == '__main__':
     app.run()
